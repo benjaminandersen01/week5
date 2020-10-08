6cf288a41db7f897ac53c89d98dbe4c15eea9269
Benjamin Scott Andersen <64924716+benjaminandersen01@users.noreply.github.com>
2020-10-08 18:37:37 -0500

    Add files via upload

--- monsters.js
+++ monsters.js
@@ -3,16 +3,35 @@ var mobs = [
 	name: "Gelatinous Cube",
 	hp: 8,
 	damage: 2,
+	image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Findigowarrior%2Fart%2FGeeks-Collaborative-Gaming-D6-Gelatinous-Cube-828616713&psig=AOvVaw2kb1n3FBFJlg6n9B2QdK1G&ust=1602165828465000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCAk_zSouwCFQAAAAAdAAAAABAD",
 	},
 	{
 	name: "Orc",
 	hp: 35,
 	damage: 13,
+	image: "https://sites.google.com/site/npgsphere/_/rsrc/1381943357178/character/race/bosek/orc/race-orc2.PNG?height=400&width=340",
 	},
 	{
 	name: "Snake King",
 	hp: 72,
 	damage: 25,
+	image: "https://i.pinimg.com/originals/bb/50/43/bb5043df1ac0b108df3c74f6faff77ec.jpg"
 	}
-	]
+	];
+	/*
+	var images = [
+		{
+		name: "Gelatinous Cube",
+		 image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Findigowarrior%2Fart%2FGeeks-Collaborative-Gaming-D6-Gelatinous-Cube-828616713&psig=AOvVaw2kb1n3FBFJlg6n9B2QdK1G&ust=1602165828465000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCAk_zSouwCFQAAAAAdAAAAABAD",
+		},
+		{
+		name: "Orc",
+		 image: "https://sites.google.com/site/npgsphere/_/rsrc/1381943357178/character/race/bosek/orc/race-orc2.PNG?height=400&width=340",
+		},
+		{
+		name: "Snake King",
+		 image: "https://i.pinimg.com/originals/bb/50/43/bb5043df1ac0b108df3c74f6faff77ec.jpg"
+		}
+		];
+		*/
 
